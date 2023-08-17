6858fe5358b1e424fe8e88466823927eb2774206
zachlamont <zachlamont1@gmail.com>
2023-08-17 22:05:18 +1000

    Complete Part 3 up to "Application-level express middleware"

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import "dotenv/config";
 import cors from "cors";
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 console.log("Hello Node.js project.");
@@ -46,8 +49,38 @@ app.get("/users/:userId", (req, res) => {
   return res.send(users[req.params.userId]);
 });
 
+app.get("/messages", (req, res) => {
+  return res.send(Object.values(messages));
+});
+
+app.get("/messages/:messageId", (req, res) => {
+  return res.send(messages[req.params.messageId]);
+});
+
+app.get('/session', (req, res) => {
+    return res.send(users[req.me.id]);
+  });
+
 app.post("/users", (req, res) => {
   return res.send("POST HTTP method on user resource");
+});
+
+app.use((req, res, next) => {
+  req.me = users[1];
+  next();
+});
+
+app.post("/messages", (req, res) => {
+  const id = uuidv4(); //generate a unique identifier for the message
+  const message = {
+    id,
+    text: req.body.text,
+    userId: req.me.id,
+  };
+
+  messages[id] = message;
+
+  return res.send(message);
 });
 
 app.put("/users/:userId", (req, res) => {
@@ -58,6 +91,17 @@ app.delete("/users/:userId", (req, res) => {
   return res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
 });
 
+app.delete("/messages/:messageId", (req, res) => {
+  const { [req.params.messageId]: message, ...otherMessages } = messages;
+
+  messages = otherMessages;
+
+  return res.send(message);
+});
+
+
+
+  
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`)
 );
