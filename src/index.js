a91b4cb19cb8d7363e6507baf2dfbdc1af094ff0
zachlamont <zachlamont1@gmail.com>
2023-08-17 21:20:35 +1000

    Complete tutorial up to 'MAKING SENSE OF REST WITH EXPRESS'

--- src/index.js
+++ src/index.js
@@ -10,31 +10,57 @@ console.log("Hello Node.js project.");
 
 console.log(process.env.MY_SECRET);
 
-app.get('/users', (req, res) => {
-    return res.send('GET HTTP method on user resource');
-  });
-  
-  app.post('/users', (req, res) => {
-    return res.send('POST HTTP method on user resource');
-  });
-  
-  app.put('/users/:userId', (req, res) => {
-    return res.send(
-      `PUT HTTP method on user/${req.params.userId} resource`,
-    );
-  });
-  
-  app.delete('/users/:userId', (req, res) => {
-    return res.send(
-      `DELETE HTTP method on user/${req.params.userId} resource`,
-    );
+let users = {
+  1: {
+    id: "1",
+    username: "Robin Wieruch",
+  },
+  2: {
+    id: "2",
+    username: "Dave Davids",
+  },
+};
+
+let messages = {
+  1: {
+    id: "1",
+    text: "Hello World",
+    userId: "1",
+  },
+  2: {
+    id: "2",
+    text: "By World",
+    userId: "2",
+  },
+};
+
+//app.get("/users", (req, res) => {
+//return res.send("GET HTTP method on user resource");
+//});
+
+app.get("/users", (req, res) => {
+  return res.send(Object.values(users));
 });
 
+app.get("/users/:userId", (req, res) => {
+  return res.send(users[req.params.userId]);
+});
+
+app.post("/users", (req, res) => {
+  return res.send("POST HTTP method on user resource");
+});
+
+app.put("/users/:userId", (req, res) => {
+  return res.send(`PUT HTTP method on user/${req.params.userId} resource`);
+});
+
+app.delete("/users/:userId", (req, res) => {
+  return res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
+});
 
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`)
 );
-
 
 /* 
 Start your Express server on the command line (npm run start), if it isn't running already, and execute four cURL commands in another command line window. You should see the following output for the commands:
@@ -52,7 +78,4 @@ curl -X DELETE http://localhost:3000
 -> Received a DELETE HTTP method
 
 By default cURL will use a HTTP GET method. However, you can specify the HTTP method with the -X flag (or --request flag).
-*/ 
-
-
-
+*/
