40a1d529dd2ee2432dfa1c069e7d462423d543c2
zachlamont <zachlamont1@gmail.com>
2023-08-17 21:06:55 +1000

    Complete part 3 up to "EXPRESS ROUTES: URIS ARE REST RESOURCES"

--- src/index.js
+++ src/index.js
@@ -10,13 +10,49 @@ console.log("Hello Node.js project.");
 
 console.log(process.env.MY_SECRET);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/users', (req, res) => {
+    return res.send('GET HTTP method on user resource');
+  });
+  
+  app.post('/users', (req, res) => {
+    return res.send('POST HTTP method on user resource');
+  });
+  
+  app.put('/users/:userId', (req, res) => {
+    return res.send(
+      `PUT HTTP method on user/${req.params.userId} resource`,
+    );
+  });
+  
+  app.delete('/users/:userId', (req, res) => {
+    return res.send(
+      `DELETE HTTP method on user/${req.params.userId} resource`,
+    );
 });
+
 
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`)
 );
+
+
+/* 
+Start your Express server on the command line (npm run start), if it isn't running already, and execute four cURL commands in another command line window. You should see the following output for the commands:
+
+curl http://localhost:3000
+-> Received a GET HTTP method
+
+curl -X POST http://localhost:3000
+-> Received a POST HTTP method
+
+curl -X PUT http://localhost:3000
+-> Received a PUT HTTP method
+
+curl -X DELETE http://localhost:3000
+-> Received a DELETE HTTP method
+
+By default cURL will use a HTTP GET method. However, you can specify the HTTP method with the -X flag (or --request flag).
+*/ 
 
 
 
